dc9f24cc156ef4988956fea8574db270008b9577
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2022-10-28 18:15:31 +0600

    category added

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express')
 const app = express();
+const cors = require('cors')
 const port = process.env.port || 5000;
 
+const Allcategories = require('./data/category.json');
+// const course = require('./data/courses.json');
 
-app.get('/', (req, res) => {
-    res.send('Courses Api Runnibg');
+
+app.use(cors());
+
+app.get('/categories', (req, res) => {
+    res.send(Allcategories);
 });
+
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id;
+    const details = Allcategories.find(detail => detail.id == id);
+    res.send(details);
+});
+
+
 
 app.listen(port, () => {
     console.log('Education Server ', port);
 })
+
